f03c30f3b6038d53db674d612ac0cf01bf3b5149
AlexHouwaard <94444669+AlexHouwaard@users.noreply.github.com>
2022-09-13 14:16:56 +0200

    updates for movement

--- js/javascript.js
+++ js/javascript.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 
 let snakeX = 200;
 let snakeY = 200;
+let direction = 'null';
 
 function drawBackground(){
 ctx.fillStyle = "black";
@@ -14,11 +15,37 @@ ctx.fillStyle = "green";
 ctx.fillRect(snakeX, snakeY, 20, 20);
 }
 
+function changeDirection(event){
+    if(event.code == 'ArrowUp') {
+        direction = 'up';
+    } else if (event.code == 'ArrowDown'){
+        direction = 'down';
+    } else if (event.code == 'ArrowRight'){
+        direction = 'right';
+    }else if (event.code == 'ArrowLeft'){
+        direction = 'left';
+    }
+}
 function update() {
-    snakeX += 20;
+    if (direction == 'right'){
+        snakeX += 20;
+    } else if (direction == 'left'){
+        snakeX -=20;
+    } else if (direction == 'up'){
+        snakeY -=20;
+    } else if (direction == 'down') 
+    {snakeY +=20;
+    }
+    addEventListener('keydown',changeDirection);
     drawBackground();
     drawSnake();
     
 }
 
-setInterval(update, 1000);
+/*
+drawBackground();
+drawSnake();
+
+*/
+
+setInterval(update, 250);
